4c8e79d8c9b96a2f209155a2fbf96581d7c92a1a
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-11-23 17:57:48 -0500

    hatch init commit

---
+++ src/crec/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
+#
+# SPDX-License-Identifier: MIT
